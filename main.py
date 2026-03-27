3b6368a38a9ec3a1f7acba2444dab24096c688ce
Knarg <knarg0000@gmail.com>
2026-03-27 08:02:03 +0200

    Refactor: streamlining internal logic

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+# --- SYNC DATA BLOCK: INSPECT ---
+        kwargs = {}
+        kwargs_started = False
+        for param_name, param in self._signature.parameters.items():
+            if not kwargs_started:
+                if param.kind in (_VAR_KEYWORD, _KEYWORD_ONLY):
+
+# --- END OF NODE UPDATE ---
